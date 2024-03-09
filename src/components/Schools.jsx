3faec7fe54c271c975a1f8ea32b51c825a9ddcce
alejandro83457 <martinezalejandro2001@hotmail.com>
2024-03-08 19:25:58 -0800

    fix: add correct and mostly final styling to forms

--- src/components/Schools.jsx
+++ src/components/Schools.jsx
@@ -10,38 +10,40 @@ function Schools({
   emptySchoolsFlag,
 }) {
   return (
-    <div>
-      {Object.entries(schoolsData).map(([key, value]) => (
-        <SchoolForm
-          key={key}
-          id={key}
-          handleForm={handleForm}
-          schoolData={value}
-          handleDeleteForm={handleDeleteForm}
-          emptySchoolsFlag={emptySchoolsFlag}
-        />
-      ))}
-      <div className="options">
-        <input
-          type="button"
-          value="new"
-          onClick={() => handleNewForm('schoolData')}
-        />
-        <input
-          type="button"
-          value="edit"
-          onClick={() => handleEditForm('schoolData')}
-        />
-        <input
-          type="button"
-          value="add"
-          onClick={(e) => handleSubmit(e, 'schoolData')}
-        />
+    <>
+      <div id="school-forms">
+        {Object.entries(schoolsData).map(([key, value]) => (
+          <SchoolForm
+            key={key}
+            id={key}
+            handleForm={handleForm}
+            schoolData={value}
+            handleDeleteForm={handleDeleteForm}
+            emptySchoolsFlag={emptySchoolsFlag}
+          />
+        ))}
+        <div className="options">
+          <input
+            type="button"
+            value="new"
+            onClick={() => handleNewForm('schoolData')}
+          />
+          <input
+            type="button"
+            value="edit"
+            onClick={() => handleEditForm('schoolData')}
+          />
+          <input
+            type="button"
+            value="add"
+            onClick={(e) => handleSubmit(e, 'schoolData')}
+          />
+        </div>
       </div>
       <div className="errorMessage">
         {emptySchoolsFlag && '* Fill out all fields'}
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -13,41 +13,43 @@ function Works({
   emptyWorksFlag,
 }) {
   return (
-    <div>
-      {Object.entries(worksData).map(([key, value]) => (
-        <WorkForm
-          key={key}
-          id={key}
-          handleForm={handleForm}
-          workData={value}
-          handleDeleteForm={handleDeleteForm}
-          addDuty={addDuty}
-          updateDuty={updateDuty}
-          deleteDuty={deleteDuty}
-          emptyWorksFlag={emptyWorksFlag}
-        />
-      ))}
-      <div className="options">
-        <input
-          type="button"
-          value="new"
-          onClick={() => handleNewForm('workData')}
-        />
-        <input
-          type="button"
-          value="edit"
-          onClick={() => handleEditForm('workData')}
-        />
-        <input
-          type="button"
-          value="add"
-          onClick={(e) => handleSubmit(e, 'workData')}
-        />
+    <>
+      <div id="work-forms">
+        {Object.entries(worksData).map(([key, value]) => (
+          <WorkForm
+            key={key}
+            id={key}
+            handleForm={handleForm}
+            workData={value}
+            handleDeleteForm={handleDeleteForm}
+            addDuty={addDuty}
+            updateDuty={updateDuty}
+            deleteDuty={deleteDuty}
+            emptyWorksFlag={emptyWorksFlag}
+          />
+        ))}
+        <div className="options">
+          <input
+            type="button"
+            value="new"
+            onClick={() => handleNewForm('workData')}
+          />
+          <input
+            type="button"
+            value="edit"
+            onClick={() => handleEditForm('workData')}
+          />
+          <input
+            type="button"
+            value="add"
+            onClick={(e) => handleSubmit(e, 'workData')}
+          />
+        </div>
       </div>
       <div className="errorMessage">
         {emptyWorksFlag && '* Fill out all fields'}
       </div>
-    </div>
+    </>
   );
 }
 export { Works };
